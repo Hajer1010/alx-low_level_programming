eaa9d71d9e25c422cfc11042197b09f0cc729bd1
hajer <hajeer_00@hotmail.com>
2023-09-07 05:56:40 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /**
  * main -Entry point
- * Return: 1(fail)
+ * Return: 1(success)
  */
 int main(void)
-{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
