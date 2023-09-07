b759764722a02ad9075e92b9f666513c26ed4f3f
hajer <hajeer_00@hotmail.com>
2023-09-07 05:11:23 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main -  A C program print with put function
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main -entry point
- *
- * description :prints with printf function
- * 
- * return : 0 
-*/
+ * Description :prints with printf function
+ * Return: 0 (Success)
+ */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+{printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
 }
