5268e275ed1f03f7ac0d192dc57eb7bfdb9b2ced
hajer <hajeer_00@hotmail.com>
2023-09-07 05:53:57 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main -Entry point
  * Return: 1(fail)
  */
 int main(void)
-{char qouts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1.qouts.59)
+{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
 return (1);
 }
