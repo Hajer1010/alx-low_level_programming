8590e4ad5d0f61157453f9130b3676dbf9700f1d
hajer <hajeer_00@hotmail.com>
2023-09-07 04:06:54 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -ebtry point
+ *
+ * description : print with put function
+ *
+ * return : 0 (success)
+ */ 
+int main()
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
