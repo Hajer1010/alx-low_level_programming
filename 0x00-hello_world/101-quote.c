644249d9a4afe1542fa7c1290205cf2ae86f0c7d
hajer <hajeer_00@hotmail.com>
2023-09-07 05:47:09 -0700

    hi

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Return: 1(fail)
+ */
+int main(void)
+{char qouts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1.qouts.59)
+return (1);
+}
