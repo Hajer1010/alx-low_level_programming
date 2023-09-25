4aeb8e23c10bcc73a55d984cf981e418df767f5c
hajer <hajeer_00@hotmail.com>
2023-09-25 12:18:27 -0700

    done

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,11 +12,14 @@ char *_strstr(char *haystack, char *needle)
 		char *p1 = haystack;
 		char *p2 = needle;
 
-		for (*p1 == *p2 && *p2 != '\0'; p1++, p2++)
-		{
-			if (*p2 == '\0')
-				return (haystack);
-		}
+	while (*p1 == *p2 && *p2 != '\0')
+	{
+		p1++;
+		p2++;
+	}
+		if (*p2 == '\0')
+			return (haystack);
+		
 	}
 	return (0);
 }
