593a85c0392f642cd5fc3b76070459fca9eb682a
hajer <hajeer_00@hotmail.com>
2023-09-25 11:23:44 -0700

    done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int a = 0;
+	unsigned int a;
 
-	while (s[a] >= '\p')
+	for (a = 0; s[a] >= '\0'; a++)
 	{
 		if (s[a] == c)
 		{
 			return (s + a);
 		}
-		a++;
 	}
 	return ('\0');
 }
