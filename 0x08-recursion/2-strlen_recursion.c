f083dd4ed93107566c64b36b6dcc717c9f95cf62
hajer <hajeer_00@hotmail.com>
2023-09-28 10:59:28 -0700

    done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,11 @@ int _strlen_recursion(char *s)
 {
 	int length = 0;
 
-	if (*s > '\0')
+	if (*s)
 	{
-		length += _strlen_recursion(s + 1) + 1;
+		length++;
+		length += _strlen_recursion(s + 1);
 	}
-	return (lenght);
+
+	return (length);
 }
