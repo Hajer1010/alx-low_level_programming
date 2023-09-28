7861fd42936dc51d5edd3bc1f263317015634091
hajer <hajeer_00@hotmail.com>
2023-09-28 13:56:13 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,24 +4,15 @@
  * @n: num
  * Return: 0 1
  */
-int prime_num(int n, int i);
 int is_prime_number(int n)
 {
-	return (prime_num(n, 2));
+	int i;
+	for (i == 2; i <= n; i++)
+	{
+		if ( n % a == 0)
+			return (0);
+		else
+			return (1);
+	}
+	return (is_prime_number(n, i + 1));
 }
-/**
- * prime_num - checks prime num
- * @i: num
- * @n: num
- * Return: int
- */
-int prime_num(int n, int i)
-{
-	if (n % i == 0 || n <= 1)
-		return (0);
-	else if (i >= n && n > 1)
-		return (1);
-	else
-		return (prime_number(n, i + 1));
-}
-
