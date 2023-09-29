10331491e3d9bcaaeda6e750c5dfcc4b96867407
hajer <hajeer_00@hotmail.com>
2023-09-29 04:36:29 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,10 @@ int is_prime_number(int n)
 {
 	int i;
 
-	for (i = 2; i <= n; i++)
+	for (i = 2; i <= n - 1; i++)
 	{
 		if (n % i == 0)
 			return (0);
-		else
-			return (1);
 	}
+	return (1);
 }
