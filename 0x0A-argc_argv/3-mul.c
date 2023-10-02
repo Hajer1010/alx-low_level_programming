f57b4cd20b7f4fd7a4b75a06fe114111e89c0082
hajer <hajeer_00@hotmail.com>
2023-10-02 06:10:25 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-entrypint
+ * @argv:string
+ * @argc: int
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int mult;
+
+	if (argc == 3)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-entrypint
+ * @argv:string
+ * @argc: int
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	char *s;
+
+	while (--argc)
+	{
+		for (s = argv[argc]; *s; s++)
+			if (*s < '0' || *s > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
