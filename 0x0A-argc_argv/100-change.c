e00afea1e2c253b0b3d2fdf72301811cd77a43b1
Hajer1010 <hajeer_00@hotmail.com>
2023-10-02 13:41:54 -0700

    done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-entrypint
+ * @argv:string
+ * @argc: int
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, mini = 0, m = atoi(argv[1]);
+		int c = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (m >= c[i])
+			{
+				mini += m / c[i];
+				m = m % c[i];
+				if (m % c[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", mini);
+	}
+	return (0);
+}
