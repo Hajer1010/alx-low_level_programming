4fe87132bd323a641cd77a004b210f4c88fe39f5
Hajer1010 <hajeer_00@hotmail.com>
2023-10-02 13:53:04 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,14 +4,14 @@
  * main-entrypint
  * @argv:string
  * @argc: int
- * Return: 0
+ * Return: 0 1
  */
 int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
 		int i, mini = 0, m = atoi(argv[1]);
-		int c = {25, 10, 5, 2, 1};
+		int c[] = {25, 10, 5, 2, 1};
 
 		for (i = 0; i < 5; i++)
 		{
