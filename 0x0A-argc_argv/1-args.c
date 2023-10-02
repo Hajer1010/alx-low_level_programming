335a61f3089585521df7a5d439a60f1ebb39fe0d
hajer <hajeer_00@hotmail.com>
2023-10-02 05:16:34 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * @argc: counter
+ * @argv: string
+ * Return: 0 always
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%d\n", argc);
+	return (0);
+}
