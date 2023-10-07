c37fe9ebef81455a8ef5a16b26130b9acebaa3a7
Hajer1010 <hajeer_00@hotmail.com>
2023-10-07 09:58:08 -0700

    done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @n: bytes
+ * @b: const
+ * @s: pointer to the memory area
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (ptr);
+}
+/**
+ * *_calloc-entry point
+ * @nmemb: int
+ * @size: size
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	void *p;
+
+	p = malloc(sizeof(int) * nmemb);
+
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	_memset(p, 0, sizeof(int) * nmemb);
+	return (p);
+}
