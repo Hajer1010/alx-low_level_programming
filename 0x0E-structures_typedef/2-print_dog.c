0a656477ea3c59be07303a9f7f8f90e100031b0a
Hajer1010 <hajeer_00@hotmail.com>
2023-10-10 10:40:49 -0700

    done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog-entry point
+ * @d: struct
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
+
