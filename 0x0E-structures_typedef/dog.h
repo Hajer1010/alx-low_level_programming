3e99882cf9a1eca7c0ca077104850d159f7932e6
Hajer1010 <hajeer_00@hotmail.com>
2023-10-10 09:35:38 -0700

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog-structure
+ * @name: ptr
+ * @age: float
+ * @owner: ptr
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
