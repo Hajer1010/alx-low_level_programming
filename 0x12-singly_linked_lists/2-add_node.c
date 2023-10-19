6194cc631efbb74da4e15025c074513a2f343be8
hajer <hajeer_00@hotmail.com>
2023-10-19 14:34:32 -0700

    done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,14 @@
 #include "lists.h"
-
+/**
+ * add_node - function
+ * @head: ptr
+ * @str: str
+ * Return: length
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+
 	if (!head || !new)
 		return (NULL);
 	if (str)
