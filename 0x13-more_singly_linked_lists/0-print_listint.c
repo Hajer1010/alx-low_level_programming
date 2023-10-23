c1a4d7c1d30bdb2b3076b20b1ce151e63e21e1c4
hajer <hajeer_00@hotmail.com>
2023-10-23 11:46:23 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,10 +8,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t c = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-	}
 		h = h->next;
 		c++;
 	}
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,7 +25,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			node = node->next;
 		node->next = new;
 	}
-	else
-		*head = new;
 	return (new);
 }
