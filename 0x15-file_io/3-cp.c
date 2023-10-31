3601446546a2cdde0df1556b3d092bfeaa436568
hajer <hajeer_00@hotmail.com>
2023-10-30 17:22:01 -0700

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,47 +18,32 @@ int main(int ac, char **av)
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, USAGE);
-		exit(97);
-	}
+	{	dprintf(STDERR_FILENO, USAGE);
+		exit(97); }
 	from = open(av[1], O_RDONLY);
 	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]);
-		exit(98);
-	}
+	{	dprintf(STDERR_FILENO, ERR_NOREAD, av[1]);
+		exit(98); }
 	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSION);
 	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]);
-		exit(99);
-	}
+	{	dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]);
+		exit(99); }
 	while ((b = read(from, buf, READ_BUF_SIZE)) > 0)
 	{
 		if (write(to, buf, b) != b)
-		{
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]);
-			exit(99);
-		}
+		{	dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]);
+			exit(99); }
 	}
 	if (b == -1)
-	{
-		 dprintf(STDERR_FILENO, ERR_NOREAD, av[1]);
-		 exit(98);
-	}
+	{	dprintf(STDERR_FILENO, ERR_NOREAD, av[1]);
+		exit(98); }
 	from = close(from);
 	to = close(to);
 	if (from)
-	{
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from);
-		exit(100);
-	}
+	{	dprintf(STDERR_FILENO, ERR_NOCLOSE, from);
+		exit(100); }
 	if (to)
-	{
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from);
-		exit(100);
-	}
-
+	{	dprintf(STDERR_FILENO, ERR_NOCLOSE, from);
+		exit(100); }
 	return (EXIT_SUCCESS);
 }
